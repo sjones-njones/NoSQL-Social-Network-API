343f286c0007a4739755c85ebc5c5ab32a5a0875
stephanie jones <sjones.njones@gmail.com>
2023-10-07 08:18:41 -0500

    added all models

--- models/user.js
+++ models/user.js
@@ -1,19 +1,39 @@
 const { Schema, model } = require('mongoose');
 
+const validateEmail = function(email) {
+  const re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  return re.test(email)
+};
+
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: String,
-    last: String,
-    age: Number,
-  //  ??????
-   
-    videos: [
+   username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+   },
+   email: {
+    type: String,
+    required: 'Email address is required',
+    unique: true,
+    validate: [validateEmail, 'Please use a valid email address'],
+    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please use a valid email address',
+  ],
+   },   
+    thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'video',
+        ref: 'thought',
       },
     ],
+    friends: [
+      {
+        type: Schema.type.ObjectId, 
+        ref: 'user',
+      }
+    ]
   },
   {
     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
@@ -27,16 +47,10 @@ const userSchema = new Schema(
 
 // Create a virtual property `fullName` that gets and sets the user's full name
 userSchema
-  .virtual('fullName')
+  .virtual('friendCount')
   // Getter
   .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
+    return this.friends.length;
   });
 
 // Initialize our User model
